d198b45315e328ca79136e7567bf0629e6eada2e
taedium8vitae <gabguinn@bluewin.ch>
2022-03-13 21:53:51 +0100

    changins files order

--- index.php
+++ index.php
@@ -1,50 +1,50 @@
 <?php
-//OPTIONS
-$db_host = "localhost";
-$db_name = "poll";
-$db_username = "root";
-$db_password = "roo";
-$errorPage = "google.com";
-
-
-$maintenance = false;
-try{
-$mysqlConnection = new PDO(
-    'mysql:host='.$db_host.';dbname='.$db_name.';charset=utf8',
-    $db_username,
-    $db_password
-);
-}
-catch (Exception $e)
+try
 {
-    $maintenance = true;
-    die();
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=poll;charset=utf8', 'root', 'root');
 }
-if($maintenance == true) {
-    ?>
-        <!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Shineonpoll</title>
-    </head>
-    <body>
-        <h1>swag</h1>
-    </body>
-</html>
-    <?php
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+$sqlQuery = 'SELECT entries FROM entries_nb';
+$entriesStatement = $mysqlClient->prepare($sqlQuery);
+$entriesStatement->execute();
+$entries = $entriesStatement->fetchAll();
+foreach ($entries[0] as $entry) {
+    $v1_entries = $entry;
+}
+foreach ($entries[1] as $entry) {
+    $v2_entries = $entry;
+}
+if($v1_entries > $v2_entries) {
+    //header("Location: link to v2");
+    //die();
+    echo "v2";
+    $sqlQuery = 'UPDATE entries_nb SET entries = entries + 1 WHERE id = 2';
+        $insertEntry = $mysqlClient->prepare($sqlQuery);
+        $insertEntry->execute();
+}elseif($v2_entries > $v1_entries) {
+    $sqlQuery = 'UPDATE entries_nb SET entries = entries + 1 WHERE id = 1';
+        $insertEntry = $mysqlClient->prepare($sqlQuery);
+        $insertEntry->execute();
+    //header("Location: link to v1");
+    //die();
+    echo "v1";
 }else{
-?>
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Shineonpoll</title>
-    </head>
-    <body>
-        <h1>swag</h1>
-    </body>
-</html>
-<?php
+    $rand_v = rand(1, 2);
+    echo "v".$rand_v." (random)";
+    if($rand_v == 1) {
+        $sqlQuery = 'UPDATE entries_nb SET entries = entries + 1 WHERE id = 1';
+        $insertEntry = $mysqlClient->prepare($sqlQuery);
+        $insertEntry->execute();
+        //header("Location: link to v1");
+        //die();
+    }else{
+        $sqlQuery = 'UPDATE entries_nb SET entries = entries + 1 WHERE id = 2';
+        $insertEntry = $mysqlClient->prepare($sqlQuery);
+        $insertEntry->execute();
+        //header("Location: link to v2");
+        //die();
+    }
 }
-?>
